d818dde5c3a086c90a34b075c44d5e8056c731b8
Mathieu YAHIA-AMAR <92095174+Matithieuu@users.noreply.github.com>
2023-04-04 21:51:28 +0200

    Update importJSON.py

--- importJSON.py
+++ importJSON.py
@@ -6,32 +6,30 @@ url = "https://apiv3.apifootball.com/?action=get_players&player_name=Messi&APIke
 response = requests.get(url)
 
 if response.status_code == 200:
-
     data = response.json()
-
-    # Créer une liste pour stocker les données
     players = []
 
-    # Définir un compteur d'ID
-    id_counter = 1
-
-    # Parcourir les données pour extraire les attributs que vous souhaitez récupérer
     for player in data:
         nom = player["player_name"]
-        equipe = player["team_name"]
+        club = player["team_name"]
         poste = player["player_type"]
         age = player["player_age"]
         numero = player["player_number"]
 
-        # Ajouter les attributs extraits à la liste avec l'ID auto-incrémenté
-        players.append([id_counter, nom, equipe, poste, age, numero])
-        
-        # Incrémenter l'ID
-        id_counter += 1
+        players.append({
+            "id": len(players) + 1,
+            "nom": nom,
+            "club": club,
+            "numero": numero,
+            "poste": poste
+        })
 
-    # Écrire les données dans un fichier JSON
+    output = {
+        "joueurs": players
+    }
+
     with open("joueurs.json", "w") as f:
-        json.dump(players, f)
+        json.dump(output, f, indent=2)
 
 else:
     print("Une erreur s'est produite lors de la demande.")
